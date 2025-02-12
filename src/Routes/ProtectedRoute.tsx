bab7f0887fc16dfbb39c385520cfebbf4371d1d2
Obinna Anosike <smartlify09@gmail.com>
2025-02-12 10:42:19 +0100

    chore: remove unnecessary profile check hook

--- src/Routes/ProtectedRoute.tsx
+++ src/Routes/ProtectedRoute.tsx
@@ -1,11 +1,9 @@
 import { type ReactNode } from 'react';
-import { useProfileCheck } from '../hooks/useProfileCheck';
 import { useAuthContext } from '../context/AuthContext';
 import { Navigate } from 'react-router';
 
 const ProtectedRoute = ({ children }: { children: ReactNode }) => {
   const { user, loading } = useAuthContext();
-  useProfileCheck();
 
   if (!user && !loading) {
     return <Navigate to={'/signin'} />;
